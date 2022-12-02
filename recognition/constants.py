af76bdc396749a9db1764ec43f431a8091c375b3
alvesmatheus <matheus@odd.group>
2022-12-01 21:30:04 -0300

    Add constants

---
+++ recognition/constants.py
@@ -0,0 +1,15 @@
+import os
+
+ANCHOR_PATH = os.path.join('data', 'anchor')
+NEGATIVE_PATH = os.path.join('data', 'negative')
+POSITIVE_PATH = os.path.join('data', 'positive')
+
+LABELED_FACES_PATH = os.path.join('lfw', '')
+
+TRAIN_CHECKPOINTS_PATH = os.path.join('train_checkpoints', '')
+
+APP_INPUT_IMAGE_PATH = os.path.join('app', 'input.jpg')
+APP_VERIFICATION_PATH = os.path.join('app', 'verification')
+
+DETECTION_THRESHOLD = 0.7
+VERIFICATION_THRESHOLD = 0.7
